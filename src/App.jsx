eee657640998ee681f12e4ec82a45a7c2747de3e
neshanth <59483685+neshanth@users.noreply.github.com>
2024-01-31 20:13:04 +0530

    added logo, hero image and bg color

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,15 @@
+import heroImg from "./assets/hero-image-wr.jpg";
+import Logo from "./components/Logo/Logo";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <main className="main">
+        <div className="logo">
+          <Logo />
+        </div>
+        <img src={heroImg} alt="hero" className="hero-img" />
+      </main>
+    </div>
+  );
 };
 export default App;
