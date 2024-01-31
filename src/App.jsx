ac8531c1c99bed4b4027def7e926982ed11b6344
neshanth <59483685+neshanth@users.noreply.github.com>
2024-01-31 20:47:09 +0530

    added context

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import heroImg from "./assets/hero-image-wr.jpg";
 import Logo from "./components/Logo/Logo";
+import { AppContext } from "./context/AppContext";
 const App = () => {
   return (
-    <div>
-      <main className="main">
-        <div className="logo">
-          <Logo />
-        </div>
-        <img src={heroImg} alt="hero" className="hero-img" />
-      </main>
-    </div>
+    <AppContext.Provider value={{ test: "123" }}>
+      <div>
+        <main className="main">
+          <div className="logo">
+            <Logo />
+          </div>
+          <img src={heroImg} alt="hero" className="hero-img" />
+        </main>
+      </div>
+    </AppContext.Provider>
   );
 };
 export default App;
